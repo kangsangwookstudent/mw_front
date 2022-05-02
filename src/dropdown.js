65d7070577c9310b2a233d52b2d5244c991932d2
Hyerini <rinlee2001@naver.com>
2022-05-02 16:35:28 +0900

    add dropdown

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,3 +1,4 @@
+//mouseover시 드롭다운 slideDown
 $(function(){
     $('.dropdown1').hide();
     $('.nav1').hover(function(){
@@ -5,16 +6,10 @@ $(function(){
         $('.dropdown3').hide();
         $('.dropdown4').hide();
         $('.dropdown5').hide();
-        $(this).parent().find('.dropdown1').stop().slideDown(500);
-        $(this).parent().hover(function(){
-
-        }, function(){
-            $(this).parent().find('.dropdown1').slideUp(800);
-        }), function(){
-            $(this).parent().hover('.nav1 a').css('color', 'silver');
-        }, function(){
-            $('.dropdown1').hover('.dropdown1').css('display', 'block');
-        }
+        $(this).parent().find('.dropdown1').stop().slideDown(700); 
+    });
+    $('.dropdown1').mouseleave(function(){
+        $(this).parent().find('.dropdown1').stop().slideUp(900);
     });
 });
 
@@ -25,11 +20,10 @@ $(function(){
         $('.dropdown3').hide();
         $('.dropdown4').hide();
         $('.dropdown5').hide();
-        $(this).parent().find('.dropdown2').slideDown(500);
-        $(this).parent().hover(function(){
-        }, function(){
-            $(this).parent().find('.dropdown2').slideUp(900);
-        })
+        $(this).parent().find('.dropdown2').stop().slideDown(700);
+    });
+    $('.dropdown2').mouseleave(function(){
+        $(this).parent().find('.dropdown2').stop().slideUp(900);
     });
 });
 
@@ -40,11 +34,10 @@ $(function(){
         $('.dropdown2').hide();
         $('.dropdown4').hide();
         $('.dropdown5').hide();
-        $(this).parent().find('.dropdown3').slideDown(500);
-        $(this).parent().hover(function(){
-        }, function(){
-            $(this).parent().find('.dropdown3').slideUp(900);
-        })
+        $(this).parent().find('.dropdown3').stop().slideDown(700);
+    });
+    $('.dropdown3').mouseleave(function(){
+        $(this).parent().find('.dropdown3').stop().slideUp(900);
     });
 });
 
@@ -55,11 +48,10 @@ $(function(){
         $('.dropdown2').hide();
         $('.dropdown3').hide();
         $('.dropdown5').hide();
-        $(this).parent().find('.dropdown4').slideDown(500);
-        $(this).parent().hover(function(){
-        }, function(){
-            $(this).parent().find('.dropdown4').slideUp(900);
-        })
+        $(this).parent().find('.dropdown4').stop().slideDown(700);
+    });
+    $('.dropdown4').mouseleave(function(){
+        $(this).parent().find('.dropdown4').stop().slideUp(900);
     });
 });
 
@@ -71,10 +63,15 @@ $(function(){
         $('.dropdown2').hide();
         $('.dropdown3').hide();
         $('.dropdown4').hide();
-        $(this).parent().find('.dropdown5').slideDown(500);
-        $(this).parent().hover(function(){
-        }, function(){
-            $(this).parent().find('.dropdown5').slideUp(900);
-        })
+        $(this).parent().find('.dropdown5').stop().slideDown(700);
+    });
+    $('.dropdown5').mouseleave(function(){
+        $(this).parent().find('.dropdown5').stop().slideUp(900);
     });
 });
+
+$(function(){
+    $('.login').hover(function(){
+        $(this).parent().find('.dropdown5').stop().slideUp(900);
+    })
+})
